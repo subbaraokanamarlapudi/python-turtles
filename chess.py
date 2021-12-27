6daa6d2403803b5c258f6aa52caa5b96cf814b39
subbaraokanamarlapudi <88045527+subbaraokanamarlapudi@users.noreply.github.com>
2021-12-27 12:31:04 +0530

    Add files via upload

---
+++ chess.py
@@ -0,0 +1,27 @@
+import turtle
+s = turtle.Screen()
+t = turtle.Turtle()
+#clcoding.com
+def drawSqr():
+    for i in range(4):
+        t.forward(30)
+        t.left(90)
+    t.forward(30)
+if __name__== "__main__" :
+    s.setup(600,600)
+    t.speed(100)
+    for i in range(8):
+        t.up()
+        t.setpos(0, 30*i)
+        t.down()
+
+        for j in range(8):
+            if (i + j)% 2 == 0:
+                col = 'black'
+            else:
+                col = 'white'
+            t.fillcolor(col)
+            t.begin_fill()
+            drawSqr()
+            t.end_fill()
+        t.hideturtle()
